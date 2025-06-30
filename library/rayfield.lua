74fdef92111f176c84120e2ca280f1909f7c3cf3
ScarletLAB <ecsclanmail@gmail.com>
2025-06-30 20:58:12 +0300

    Update rayfield.lua

--- library/rayfield.lua
+++ library/rayfield.lua
@@ -300,7 +300,7 @@ local RayfieldLibrary = {
 
 			ToggleBackground = Color3.fromRGB(46, 45, 61),
 			ToggleEnabled = Color3.fromRGB(111, 206, 174),
-			ToggleDisabled = Color3.fromRGB(41, 40, 65),
+			ToggleDisabled = Color3.fromRGB(71, 70, 86),
 			ToggleEnabledStroke = Color3.fromRGB(91, 186, 154),
 			ToggleDisabledStroke = Color3.fromRGB(71, 70, 86),
 			ToggleEnabledOuterStroke = Color3.fromRGB(91, 186, 154),
